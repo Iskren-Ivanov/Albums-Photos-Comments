f43ab166ea186aefc00b0c008156ff4580e2fbdb
Iskren-Ivanov <iskrentivanov@gmail.com>
2021-03-25 16:42:57 +0200

    Edit Navigation with user or not

--- src/Menu/Navigation/Navigation.jsx
+++ src/Menu/Navigation/Navigation.jsx
@@ -3,32 +3,27 @@ import { useSelector } from 'react-redux';
 import { Form, Navbar, Nav, } from 'react-bootstrap';
 import LogoutBtn from '../../Components/UI/Buttons/Logout/Logout';
 import SignInOrSignUpButton from '../../Components/UI/Buttons/SwitchSignInOrSignUp/SignInOrSignUp';
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './Navigation.css';
 
 const Navigation = () => {
     const { authData } = useSelector(store => store.auth);
-    let navWithUserOrNot = authData ?
-        (
-            <Nav className="mr-auto navLeftSide">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/contacts">Contacts</Nav.Link>
-                <Nav.Link href="/albums">Albums</Nav.Link>
-            </Nav>
-        )
-        :
-        (
-            <Nav className="mr-auto navLeftSide">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/contacts">Contacts</Nav.Link>
-            </Nav>
-        );
-
     return (
         <div>
             <Navbar bg="primary" variant="dark">
-                {navWithUserOrNot}
+                {authData ?
+                    <Nav className="mr-auto navLeftSide">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/contacts">Contacts</Nav.Link>
+                        <Nav.Link href="/albums">Albums</Nav.Link>
+                    </Nav>
+                    :
+                    <Nav className="mr-auto navLeftSide">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/contacts">Contacts</Nav.Link>
+                    </Nav>
+                }
                 <Form inline>
                     {authData && <Link color="primary" className="mr-auto  link" to={'/profile'} >Profile</Link>}
                     {authData ? <LogoutBtn /> : <SignInOrSignUpButton />}
